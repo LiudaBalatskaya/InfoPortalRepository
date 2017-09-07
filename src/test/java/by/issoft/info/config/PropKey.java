dda18dfb986668aca4b133623ad1af6ea0526b7a
andy-ka <Ar1#1234>
2017-09-07 17:24:55 +0300

    SuppressWarning is added

--- src/test/java/by/issoft/info/config/PropKey.java
+++ src/test/java/by/issoft/info/config/PropKey.java
@@ -1,11 +1,11 @@
 package by.issoft.info.config;
 
+@SuppressWarnings("WeakerAccess")
 public class PropKey {
     public static final String WEBDRIVER_GECKO_DRIVER = "webdriver.gecko.driver";
     public static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
     public static final String WEBDRIVER_CHROME_REMOTE_HUB = "webdriver.chrome.remote.hub";
     public static final String WEBDRIVER_FIREFOX_REMOTE_HUB = "webdriver.firefox.remote.hub";
-    @SuppressWarnings("WeakerAccess")
     public static final String SELENIDE_BASE_URL = "selenide.base.url";
     public static final String BROWSER = "browser";
 }
